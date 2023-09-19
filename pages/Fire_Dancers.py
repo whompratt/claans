7cb2593f69a7684c1f382bdada7d7354c6426c28
Alexander Billington <afbillington96@gmail.com>
2023-09-19 14:57:55 +0100

    Add passwords to the claan pages

--- pages/Fire_Dancers.py
+++ pages/Fire_Dancers.py
@@ -2,6 +2,35 @@ import streamlit as st
 from pathlib import Path
 from PIL import Image
 
+def check_password():
+    """Returns `True` if the user had the correct password."""
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["fire_password"]:
+            st.session_state["fire_password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["fire_password_correct"] = False
+
+    if "fire_password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+
+    elif not st.session_state["fire_password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+
+    else:
+        # Password correct.
+        return True
+
 # Load the assets for the app
 img_path = Path(__file__).parents[0]
 claan_img = Image.open(f"{img_path}/Page_Images/Flame-dancers-hex.png")
@@ -9,19 +38,20 @@ claan_img = Image.open(f"{img_path}/Page_Images/Flame-dancers-hex.png")
 # Set the page title and icon and set layout to "wide" to minimise margains
 st.set_page_config(page_title="Fire Dancers", page_icon=":fire:")
 
-# Header section
-with st.container():
-    # Create columns
-    head_l, head_r = st.columns((2.5,1))
+if check_password():
+    # Header section
+    with st.container():
+        # Create columns
+        head_l, head_r = st.columns((2.5,1))
 
-    with head_l:
-        # Add a subheader
-        st.subheader("Advancing Analytics")
-        # Add a title
-        st.title("Fire Dancers")
+        with head_l:
+            # Add a subheader
+            st.subheader("Advancing Analytics")
+            # Add a title
+            st.title("Fire Dancers")
 
-    with head_r:
-        # Add logo
-        st.image(claan_img)
+        with head_r:
+            # Add logo
+            st.image(claan_img)
 
-    st.write("Welcome to the Fire Dancers Claan area, check back in later for more goodies!")
+        st.write("Welcome to the Fire Dancers Claan area, check back in later for more goodies!")
