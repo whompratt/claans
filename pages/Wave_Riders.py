130f93746c3d7101bf277f4eb0e56efb76830899
Alex Billington <afbillington96@gmail.com>
2024-01-19 11:21:14 +0000

    WR Double quest

--- pages/Wave_Riders.py
+++ pages/Wave_Riders.py
@@ -198,9 +198,9 @@ if check_password():
             try:
                 if name == "Please select your name":
                     st.write("Please tell me who you are!")
-                elif len(temp_lookup) >= 1 and quest != "Block out at least 30 minutes per day to have lunch away from your desk":
+                elif len(temp_lookup) >= 1 and quest != "Log your time in Float! And stay on top of it!":
                     st.write("You have already completed that quest!")
-                elif quest == "Block out at least 30 minutes per day to have lunch away from your desk" and len(temp_lookup) >= 2:
+                elif quest == "Log your time in Float! And stay on top of it!" and len(temp_lookup) >= 2:
                     st.write("You have already completed that quest twice!")
                 elif submissions != [] and submissions[-1]['Date'] > datetime.datetime.now() + datetime.timedelta(seconds = -20):
                     st.write("You have already made a submission in the last 20seconds!")
