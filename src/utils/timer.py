f785436f97692759b84fba676b5b86632912822c
Jake Ratcliffe <JakeR1342@gmail.com>
2024-08-30 23:25:36 +0100

    Reformatted @timer log output for clarity

--- src/utils/timer.py
+++ src/utils/timer.py
@@ -12,7 +12,7 @@ def timer(func):
         duration = time() - start
         if duration >= 1:
             LOGGER.warning(
-                f"Long execution for {func.__name__} with inputs (\nargs:\n\t{',\n\t'.join([arg.__name__ for arg in args])}\nkwargs:\n\t{',\n\t'.join([f'{key}: {value}' for key, value in kwargs.items()])}\n): {duration}"
+                f"Long execution for {func.__name__}: {duration}.\nInputs (\nargs:\n\t{',\n\t'.join([arg.__name__ for arg in args])}\nkwargs:\n\t{',\n\t'.join([f'{key}: {value}' for key, value in kwargs.items()])})"
             )
         else:
             LOGGER.info(f"Exection time for {func.__name__}: {duration}")
