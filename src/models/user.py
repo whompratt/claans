cec774f510f8a5007b42affb8d9884ee4a32a9bc
Jake Ratcliffe <JakeR1342@gmail.com>
2024-10-08 22:36:29 +0100

    Updated user model to include plural table name and email validation

--- src/models/user.py
+++ src/models/user.py
@@ -1,6 +1,7 @@
 from typing import TYPE_CHECKING, List, Optional
 
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from email_validator import validate_email
+from sqlalchemy.orm import Mapped, mapped_column, relationship, validates
 
 from src.models.base import Base
 from src.models.claan import Claan
@@ -10,19 +11,30 @@ if TYPE_CHECKING:
 
 
 class User(Base):
-    __tablename__ = "user"
+    __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    long_name: Mapped[str] = mapped_column(nullable=False)
     name: Mapped[str] = mapped_column(nullable=False)
-    claan: Mapped[Claan] = mapped_column(nullable=False, index=True)
+    email: Mapped[str] = mapped_column(nullable=False, unique=True)
+    claan: Mapped[Claan] = mapped_column(nullable=True, index=True)
     active: Mapped[bool] = mapped_column(nullable=False, default=True)
 
     records: Mapped[List["Record"]] = relationship(
         back_populates="user", cascade="all, delete", passive_deletes=True
     )
 
-    def __init__(self, name: str, claan: Claan, active: Optional[bool] = True):
+    def __init__(
+        self,
+        long_name: str,
+        name: str,
+        email: str,
+        claan: Claan,
+        active: Optional[bool] = True,
+    ):
+        self.long_name = long_name
         self.name = name
+        self.email = email
         self.claan = claan
         self.active = active
 
@@ -31,3 +43,9 @@ class User(Base):
 
     def __str__(self):
         return f"User '{self.name}' in Claan '{self.claan.value}'"
+
+    @validates("email")
+    def validate_email(self, key, value):
+        email = validate_email(value)
+        if "advancinganalytics" in email.domain:
+            return email.normalized
