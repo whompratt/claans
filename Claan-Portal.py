da6650df0396b0be8e90a14458987e5afe5fae4e
Jake Ratcliffe <JakeR1342@gmail.com>
2024-10-08 22:37:24 +0100

    Re-enabled contents of home page

--- Claan-Portal.py
+++ Claan-Portal.py
@@ -78,48 +78,48 @@ def init_page() -> None:
             st.image(str(path_chart))
     # --- SCORES --- #
 
-    # st.divider()
+    st.divider()
 
-    # # --- INFO --- #
-    # with st.container():
-    #     st.header("How It Works")
-    #     st.write(
-    #         "Claans and the Claan Competition exist to encourage a healthy work-life balance, promote practising self-care, and facilitate socialisation and cooperation within Advancing Analytics."
-    #     )
-    #     st.write(
-    #         "Every fortnight, a new set of quests and activities can be completed to get points for your Claan, and as everyone knows, 'Points Mean Prizes', so make sure to get those points logged!"
-    #     )
+    # --- INFO --- #
+    with st.container():
+        st.header("How It Works")
+        st.write(
+            "Claans and the Claan Competition exist to encourage a healthy work-life balance, promote practising self-care, and facilitate socialisation and cooperation within Advancing Analytics."
+        )
+        st.write(
+            "Every fortnight, a new set of quests and activities can be completed to get points for your Claan, and as everyone knows, 'Points Mean Prizes', so make sure to get those points logged!"
+        )
 
-    #     st.subheader("Quests")
-    #     st.write(
-    #         "Quests are the first way you can earn points for your Claan. Every fortnight, a set of 5 quests will be made available for completion."
-    #     )
-    #     st.write(
-    #         "Each quest will offer a dice as a reward, with the number of sides on the dice corresponding to the quest difficulty."
-    #     )
-    #     st.write(
-    #         "After completing a quest, the dice is automatically rolled and the result is added to your Claan's score."
-    #     )
-    #     st.write(
-    #         "D4-D10: quests with a reward between D4 and D10 can be completed _daily_, meaning there are a lot of points available!"
-    #     )
-    #     st.write(
-    #         "D12: quests with a reward of D12 are considered a 'Claan Challenge', and can only be submitted once per person per fortnight."
-    #     )
+        st.subheader("Quests")
+        st.write(
+            "Quests are the first way you can earn points for your Claan. Every fortnight, a set of 5 quests will be made available for completion."
+        )
+        st.write(
+            "Each quest will offer a dice as a reward, with the number of sides on the dice corresponding to the quest difficulty."
+        )
+        st.write(
+            "After completing a quest, the dice is automatically rolled and the result is added to your Claan's score."
+        )
+        st.write(
+            "D4-D10: quests with a reward between D4 and D10 can be completed _daily_, meaning there are a lot of points available!"
+        )
+        st.write(
+            "D12: quests with a reward of D12 are considered a 'Claan Challenge', and can only be submitted once per person per fortnight."
+        )
 
-    #     st.subheader("Steps")
-    #     st.write(
-    #         "Steps are back! If you complete 10,000 or more steps in a day, then you can submit that task and get a D4!"
-    #     )
+        st.subheader("Steps")
+        st.write(
+            "Steps are back! If you complete 10,000 or more steps in a day, then you can submit that task and get a D4!"
+        )
 
-    #     st.subheader("Activities")
-    #     st.write(
-    #         "Activites are back too! Every day you can earn points for being active, with a D6, D8, and D10 available for 45 minutes of indoor, outdoor, or team sport participation respectively."
-    #     )
-    #     st.write(
-    #         "The nature of these activities will change on a regular basis, to keep things fresh and interesting."
-    #     )
-    # # --- INFO --- #
+        st.subheader("Activities")
+        st.write(
+            "Activites are back too! Every day you can earn points for being active, with a D6, D8, and D10 available for 45 minutes of indoor, outdoor, or team sport participation respectively."
+        )
+        st.write(
+            "The nature of these activities will change on a regular basis, to keep things fresh and interesting."
+        )
+    # --- INFO --- #
 
 
 def main() -> None:
