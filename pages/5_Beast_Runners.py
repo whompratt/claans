bab7016e543709e6dbc4cab07caaed480760d4af
Jake Ratcliffe <JakeR1342@gmail.com>
2024-10-09 12:05:11 +0100

    Moved admin page to position 7 and added pages for two new claans

---
+++ pages/5_Beast_Runners.py
@@ -0,0 +1,10 @@
+from src.models.claan import Claan
+from src.utils.claan_page import ClaanPage
+
+
+def main() -> None:
+    ClaanPage(Claan.BEAST_RUNNERS)
+
+
+if __name__ == "__main__":
+    main()
---
+++ pages/6_Iron_Stalkers.py
@@ -0,0 +1,10 @@
+from src.models.claan import Claan
+from src.utils.claan_page import ClaanPage
+
+
+def main() -> None:
+    ClaanPage(Claan.IRON_STALKERS)
+
+
+if __name__ == "__main__":
+    main()
